4688fc7986f55592771b1d0047929a0962e1c943
EdisonArias7 <edison.arias02@uptc.edu.co>
2021-08-26 16:23:35 -0500

    crear eventos con headers

--- src/app/features/admin/components/events/events.component.ts
+++ src/app/features/admin/components/events/events.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -12,11 +12,7 @@ import { Observable } from 'rxjs';
 })
 export class EventsComponent implements OnInit {
   form: FormGroup;
-
-  // private httpHeaders = new HttpHeaders({
-  //   'Content-Type': 'application/json',
-  // });
-  // private aa = JSON.stringify({ x: 'hola', y: 'mundo' });
+  SERVER_URL = "http://localhost:3000/eventss";
 
   constructor(
     public fb: FormBuilder,
@@ -29,45 +25,38 @@ export class EventsComponent implements OnInit {
       description_event: [''],
       presale: [''],
       artists: [''],
-      avatar: [null],
-    })
+      profile: ['']
+    });
   }
 
   ngOnInit() {}
 
   uploadFile(event) {
     const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({
-      avatar: file
-    });
-    this.form.get('avatar').updateValueAndValidity()
+    this.form.get('profile').setValue(file);
   }
 
   submitForm() {
-    var formData: any = new FormData();
+    let headers = new HttpHeaders();
+  //this is the important step. You need to set content type as null
+    headers.set('Content-Type', null);
+    headers.set('Accept', "multipart/form-data");
+    let params = new HttpParams();
+
+    var formData: FormData = new FormData();
     formData.append('date_event', this.form.get('date_event').value);
     formData.append('city_event', this.form.get('city_event').value);
     formData.append('direction_event', this.form.get('direction_event').value);
-    formData.append('description_event',this.form.get('description_event').value
-    );
+    formData.append('description_event',this.form.get('description_event').value);
     formData.append('presale', this.form.get('presale').value);
     formData.append('artists', this.form.get('artists').value);
-    formData.append('avatar', this.form.get('avatar').value);
+    formData.append('profile', this.form.get('profile').value);
 
-    console.log(this.form.get('date_event').value);
+    console.log(this.form.get('profile').value);
 
-    return this.http.post('http://localhost:3000/eventss', formData).subscribe(
+    this.http.post('http://localhost:3000/eventss',formData,{params,headers}).subscribe(
       (response) => console.log(response),
       (error) => console.log(error)
     )
-
-    // this.http
-    //   .post('http://localhost:3000/api/eventss', formData, {
-    //     headers: this.httpHeaders,
-    //   })
-    //   .subscribe(
-    //     (response) => console.log(response),
-    //     (error) => console.log(error)
-    //   );
   }
 }
