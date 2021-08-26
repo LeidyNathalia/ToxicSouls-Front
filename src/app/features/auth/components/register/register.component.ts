eeab1f205b07801b630bd39bd7dab04a7d3467a3
LeidyNathalia <fonsecacano.nathalia@gmail.com>
2021-08-25 21:55:34 -0500

    Registro

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  registerForm = new FormGroup({
+    ccFormControl : new FormControl('', [
+      Validators.required,
+      Validators.
+    ]),
+
+    nameFormControl : new FormControl('', [
+      Validators.required
+      // Validators.email,
+    ]),
+
+    emailFormControl : new FormControl('', [
+      Validators.required
+      // Validators.email,
+    ]),
+
+    passFormControl : new FormControl('', [
+      Validators.required
+      // Validators.email,
+    ]),
+
+  })
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
 
 }
