2ac0556848e101e66b412173aecf80618894f100
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-08-31 00:30:05 -0500

    mostrar botón de registrar usuarios

--- src/app/features/admin/components/sidebar-admin/sidebar-admin.component.ts
+++ src/app/features/admin/components/sidebar-admin/sidebar-admin.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarAdminComponent implements OnInit {
 
-  constructor() { }
+  isAdmin: boolean;
+
+  constructor() {
+    if(localStorage.getItem('role') == 'admin') {
+      this.isAdmin = true;
+    }
+   }
 
   ngOnInit(): void {
   }
