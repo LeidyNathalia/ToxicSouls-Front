209bfb2b59405e63d1b0e4bce2a9e918042b1e57
EdisonArias7 <edison.arias02@uptc.edu.co>
2021-09-09 00:42:14 -0500

    actualizacion de lista de artistas

--- src/app/features/admin/components/Artistas/view-list-artist/view-list-artist.component.ts
+++ src/app/features/admin/components/Artistas/view-list-artist/view-list-artist.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ArtistService} from '../../../../../services/user-service/artist.service';
 
 export interface ArtistData {
   id: string;
@@ -25,11 +27,24 @@ export class ViewListArtistComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {
+  constructor(
+    private artistService: ArtistService,
+    private routes: Router
+  ) {
 
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit(): Promise<any> {
+    try {
+      const result = await this.artistService.getArtist();
+      console.log('result', result);
+      this.artistsList = await result.artists;
+      console.log(result.artists);
+      console.log(this.artistsList);
+    } catch (error) {
+      console.log(error);
+    }
+    this.dataSource = new MatTableDataSource(this.artistsList);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
@@ -41,6 +56,33 @@ export class ViewListArtistComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  getArtistById(row: any): string {
+    const id = row._id;
+    return id;
+  }
+
+  async delete(row: any): Promise<any> {
+    try {
+      //const header = this.headerService.createHeader(localStorage.getItem('token'));
+      const deleteUser = await this.artistService.deleteArtist(row._id);
+      const newList = await this.artistService.getArtist();
+      for (let i = 0; i < this.artistsList.length; i++) {
+        if (this.artistsList[i]['_id'] === row._id) {
+          this.artistsList.splice(i, 1);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    this.dataSource = new MatTableDataSource(this.artistsList);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  modify(row: any) {
+    this.routes.navigate(['/admin/ModifyArtistComponent'],{queryParams:{id:this.getArtistById(row)}});
   }
 
   ngOnInit(): void {
