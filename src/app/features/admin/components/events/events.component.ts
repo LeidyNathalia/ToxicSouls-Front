9efa422b7f42adfc791f2501a50cbb2548585fa7
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-09-16 22:36:21 -0500

    arreglo cloudinary en eventos

--- src/app/features/admin/components/events/events.component.ts
+++ src/app/features/admin/components/events/events.component.ts
@@ -29,13 +29,9 @@ export class EventsComponent implements OnInit {
   nuevaFechaPreventa: FormControl = this.fb.control('', Validators.required);
   nuevoPrecioPreventa: FormControl = this.fb.control('', Validators.required);
 
-
-
-
-  get presales(){
+  get presales() {
     return this.form.get('presales') as FormArray;
   }
-
 
   constructor(
     public fb: FormBuilder,
@@ -64,9 +60,7 @@ export class EventsComponent implements OnInit {
         [Validators.required, Validators.pattern(/[A-Za-z0-9'\.\-\s\,]/)],
       ],
       //presale: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
-      presales: this.fb.array([
-
-      ], Validators.required),
+      presales: this.fb.array([], Validators.required),
       artists: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
       aforo: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
       profile: [''],
@@ -94,6 +88,7 @@ export class EventsComponent implements OnInit {
   }
 
   onUpload() {
+    console.log('aqui entra?');
     //Scape empty array
     if (!this.files[0]) {
       alert('Primero sube una imagen, por favor');
@@ -105,15 +100,14 @@ export class EventsComponent implements OnInit {
     data.append('upload_preset', 'angular_cloudinary');
     data.append('cloud_name', 'toxic-souls');
 
-    this._uploadService
-      .uploadImage(data)
-      .subscribe((response) => {
-        if (response) {
-          //console.log(response);
-          console.log('url de img', response.url);
-          this.url_cloudinary_img_current = response.url;
-        }
-      });
+    this._uploadService.uploadImage(data).subscribe(async (response) => {
+      if (response) {
+        //console.log(response);
+        console.log('url de img', response.url);
+        this.url_cloudinary_img_current = await response.url;
+        this.postData();
+      }
+    });
   }
 
   uploadFile(event) {
@@ -125,68 +119,64 @@ export class EventsComponent implements OnInit {
     this.routes.navigate(['/admin/list-event']);
   }
 
-  submitForm() {
+  postData() {
+    console.log('tiene que llegar despues de la url');
     let headers = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('hola', 'mundo');
-
     let options = { headers: headers };
-    this.postData(options);
-
+    console.log('se ejecuta?');
+    const body = {
+      date_event: this.form.get('date_event').value,
+      city_event: this.form.get('city_event').value,
+      direction_event: this.form.get('direction_event').value,
+      description_event: this.form.get('description_event').value,
+      presales: this.arrayItems,
+      artists: this.form.get('artists').value,
+      capacity: this.form.get('aforo').value,
+      flyer: this.url_cloudinary_img_current,
+    };
+    console.log('bodyyy', body);
+    this.http
+      .post<any>('http://localhost:3000/api/events/add-event', body, options)
+      .subscribe(
+        (response) => {
+          console.log('response', response);
+          this.routes.navigate(['/admin/list-event']);
+        },
+        (error) => console.log(error)
+      );
   }
 
-  postData(options) {
-    this.onUpload();
-    setTimeout(() => {
-      console.log('se ejecuta?');
-      const body = {
-        date_event: this.form.get('date_event').value,
-        city_event: this.form.get('city_event').value,
-        direction_event: this.form.get('direction_event').value,
-        description_event: this.form.get('description_event').value,
-        presales: this.arrayItems,
-        artists: this.form.get('artists').value,
-        capacity: this.form.get('aforo').value,
-        flyer: this.url_cloudinary_img_current,
-      };
-      console.log('bodyyy', body);
-      this.http
-        .post<any>('http://localhost:3000/api/events/add-event', body, options)
-        .subscribe(
-          (response) => {
-            console.log('response', response)
-            this.routes.navigate(['/admin/list-event']);
-          },
-          (error) => console.log(error)
-        );
-    }, 1500);
-  }
-
-  eliminarPreventa(i: number){
+  eliminarPreventa(i: number) {
     this.presales.removeAt(i);
-    this.arrayItems.splice(i,1);
+    this.arrayItems.splice(i, 1);
     this.demoArray.removeAt(this.demoArray.length - 1);
   }
 
-  agregarPreventan(){
-    if(this.nuevaFechaPreventa.invalid && this.nuevoPrecioPreventa.invalid){
+  agregarPreventan() {
+    if (this.nuevaFechaPreventa.invalid && this.nuevoPrecioPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       this.nuevaFechaPreventa.markAllAsTouched();
       return;
-    }else if(this.nuevaFechaPreventa.invalid){
+    } else if (this.nuevaFechaPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       return;
-    }
-    else if(this.nuevoPrecioPreventa.invalid){
+    } else if (this.nuevoPrecioPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       return;
     }
     console.log(this.nuevaFechaPreventa.value, this.nuevoPrecioPreventa.value);
-    this.arrayItems.push({date_end_presale: this.nuevaFechaPreventa.value, price_presale: this.nuevoPrecioPreventa.value});
-    this.presales.push(this.fb.control({
+    this.arrayItems.push({
       date_end_presale: this.nuevaFechaPreventa.value,
-      price_presale: this.nuevoPrecioPreventa.value
-    }));
+      price_presale: this.nuevoPrecioPreventa.value,
+    });
+    this.presales.push(
+      this.fb.control({
+        date_end_presale: this.nuevaFechaPreventa.value,
+        price_presale: this.nuevoPrecioPreventa.value,
+      })
+    );
     this.nuevaFechaPreventa.reset();
     this.nuevoPrecioPreventa.reset();
     console.log('preventan', this.presales.controls);
@@ -195,6 +185,5 @@ export class EventsComponent implements OnInit {
 
   get demoArray() {
     return this.form.get('demoArray') as FormArray;
- }
-
+  }
 }
--- src/app/features/admin/components/modify-event/modify-event.component.ts
+++ src/app/features/admin/components/modify-event/modify-event.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { HttpClient, HttpParams, HttpClientModule } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { UploadServiceModify } from './upload.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { EventService} from '../../../../services/user-service/event.service';
-
+import { EventService } from '../../../../services/user-service/event.service';
 
 export interface eventData {
   date_event: string;
@@ -20,7 +25,7 @@ export interface eventData {
 @Component({
   selector: 'app-modify-event',
   templateUrl: './modify-event.component.html',
-  styleUrls: ['./modify-event.component.scss']
+  styleUrls: ['./modify-event.component.scss'],
 })
 export class ModifyEventComponent implements OnInit {
   eventsList: eventData[];
@@ -33,10 +38,11 @@ export class ModifyEventComponent implements OnInit {
     'presales',
     'artists',
     'capacity',
-    'flyer'];
+    'flyer',
+  ];
 
   form: FormGroup;
-  SERVER_URL = "http://localhost:3000/eventss";
+  SERVER_URL = 'http://localhost:3000/eventss';
   url_cloudinary_img_current;
 
   arrayItems: {
@@ -47,56 +53,58 @@ export class ModifyEventComponent implements OnInit {
   nuevaFechaPreventa: FormControl = this.fb.control('', Validators.required);
   nuevoPrecioPreventa: FormControl = this.fb.control('', Validators.required);
 
-  get presales(){
+  get presales() {
     return this.form.get('presales') as FormArray;
   }
   constructor(
     public fb: FormBuilder,
     private http: HttpClient,
     private _uploadService: UploadServiceModify,
-    private routes : Router,
+    private routes: Router,
     private actroutes: ActivatedRoute,
     private eventService: EventService
   ) {
     this.form = this.fb.group({
       demoArray: this.fb.array([]),
-      date_event: ['',[
-        Validators.required,
-        Validators.pattern(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)]],
-      city_event: ['',[
-        Validators.required,
-        Validators.pattern(/^[a-zA-Z]+$/)]],
-      direction_event: ['',[
-        Validators.required]],
-      description_event: ['',[
-        Validators.required,
-        Validators.pattern(/[A-Za-z0-9'\.\-\s\,]/)]],
+      date_event: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/
+          ),
+        ],
+      ],
+      city_event: [
+        '',
+        [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)],
+      ],
+      direction_event: ['', [Validators.required]],
+      description_event: [
+        '',
+        [Validators.required, Validators.pattern(/[A-Za-z0-9'\.\-\s\,]/)],
+      ],
       /* presale: ['',[
         Validators.required,
         Validators.pattern(/^[0-9]+$/)]], */
-      presales: this.fb.array([
-
-        ], Validators.required),
-      artists:['',[
-        Validators.required,
-        Validators.pattern(/^[a-zA-Z]+$/)]],
-      capacity: ['',[
-        Validators.required,
-        Validators.pattern(/^[0-9]+$/)]],
-      flyer: ['']
+      presales: this.fb.array([], Validators.required),
+      artists: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      capacity: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+      flyer: [''],
     });
-    this.id_edit= this.actroutes.snapshot.queryParams.id;
+    this.id_edit = this.actroutes.snapshot.queryParams.id;
     this.getEvent();
   }
 
   ngOnInit() {
     this.arrayItems = [];
     var current_date = new Date().toISOString().split('T')[0];
-    document.getElementsByName("appo_date")[0].setAttribute('min', current_date);
+    document
+      .getElementsByName('appo_date')[0]
+      .setAttribute('min', current_date);
   }
 
   async cargarInfo() {
-
     try {
       const result = await this.eventService.getEvents();
       console.log('result', result);
@@ -108,32 +116,32 @@ export class ModifyEventComponent implements OnInit {
     }
   }
 
-  async edit():Promise<any> {
-    try{
-      this.onUpload();
-      setTimeout(async () => {
-        console.log('editMethod', this.form.value);
-        const editEvent = await this.eventService.editEvent(this.id_edit,this.form.value);
-        const newList = await this.eventService.getEvents();
-        this.routes.navigate(['/admin/list-event']);
-      }, 1000);
-    }catch (error) {
-      console.log(error)
+  async edit(): Promise<any> {
+    try {
+      console.log('editMethod', this.form.value);
+      const editEvent = await this.eventService.editEvent(
+        this.id_edit,
+        this.form.value
+      );
+      const newList = await this.eventService.getEvents();
+      this.routes.navigate(['/admin/list-event']);
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  async getEvent():Promise<any>{
+  async getEvent(): Promise<any> {
     const event = await this.eventService.getEventById(this.id_edit);
-    this.form.patchValue({date_event:event.event.date_event});
-    this.form.patchValue({city_event:event.event.city_event});
-    this.form.patchValue({direction_event:event.event.direction_event});
-    this.form.patchValue({description_event:event.event.description_event});
-    this.form.patchValue({artists:event.event.artists})
+    this.form.patchValue({ date_event: event.event.date_event });
+    this.form.patchValue({ city_event: event.event.city_event });
+    this.form.patchValue({ direction_event: event.event.direction_event });
+    this.form.patchValue({ description_event: event.event.description_event });
+    this.form.patchValue({ artists: event.event.artists });
     //this.form.patchValue({'demoArray': this.covertArrayToArrayControl(event.event.presales)});
-    console.log('press', event.event.presales)
+    console.log('press', event.event.presales);
     this.arrayItems = event.event.presales;
-    this.addPresale(event.event.presales)
-    this.form.patchValue({capacity:event.event.capacity});
+    this.addPresale(event.event.presales);
+    this.form.patchValue({ capacity: event.event.capacity });
   }
 
   /* covertArrayToArrayControl(array: string[]){
@@ -141,7 +149,7 @@ export class ModifyEventComponent implements OnInit {
       this.arrayItems.push(this.fb.control(value));
     }
   } */
-  
+
   files: File[] = [];
 
   onSelect(event) {
@@ -155,26 +163,25 @@ export class ModifyEventComponent implements OnInit {
   }
 
   onUpload() {
-    //Scape empty array
-    /* if (!this.files[0]) {
-      alert('Primero sube una imagen, por favor');
-    } */
+    if (!this.files[0]) {
+      this.edit();
+    } else {
+      const file_data = this.files[0];
+      const data = new FormData();
+      data.append('file', file_data);
+      data.append('upload_preset', 'angular_cloudinary');
+      data.append('cloud_name', 'toxic-souls');
 
-    //Upload my image to cloudinary
-    const file_data = this.files[0];
-    const data = new FormData();
-    data.append('file', file_data);
-    data.append('upload_preset', 'angular_cloudinary');
-    data.append('cloud_name', 'toxic-souls');
-
-    this._uploadService.uploadImage(data).subscribe((response) => {
-      if (response) {
-        console.log(response);
-        console.log("url de img", response.url)
-        this.url_cloudinary_img_current = response.url;
-        this.form.get('flyer').setValue(response.url);
-      }
-    });
+      this._uploadService.uploadImage(data).subscribe((response) => {
+        if (response) {
+          console.log(response);
+          console.log('url de img', response.url);
+          this.url_cloudinary_img_current = response.url;
+          this.form.get('flyer').setValue(response.url);
+          this.edit();
+        }
+      });
+    }
   }
 
   uploadFile(event) {
@@ -186,51 +193,56 @@ export class ModifyEventComponent implements OnInit {
     this.routes.navigate(['/admin/list-event']);
   }
 
-  agregarPreventan(){
-    if(this.nuevaFechaPreventa.invalid && this.nuevoPrecioPreventa.invalid){
+  agregarPreventan() {
+    if (this.nuevaFechaPreventa.invalid && this.nuevoPrecioPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       this.nuevaFechaPreventa.markAllAsTouched();
       return;
-    }else if(this.nuevaFechaPreventa.invalid){
+    } else if (this.nuevaFechaPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       return;
-    }
-    else if(this.nuevoPrecioPreventa.invalid){
+    } else if (this.nuevoPrecioPreventa.invalid) {
       this.nuevoPrecioPreventa.markAllAsTouched();
       return;
     }
     console.log(this.nuevaFechaPreventa.value, this.nuevoPrecioPreventa.value);
-    this.arrayItems.push({date_end_presale: this.nuevaFechaPreventa.value, price_presale: this.nuevoPrecioPreventa.value});
-    this.presales.push(this.fb.control({
+    this.arrayItems.push({
       date_end_presale: this.nuevaFechaPreventa.value,
-      price_presale: this.nuevoPrecioPreventa.value
-    }));
+      price_presale: this.nuevoPrecioPreventa.value,
+    });
+    this.presales.push(
+      this.fb.control({
+        date_end_presale: this.nuevaFechaPreventa.value,
+        price_presale: this.nuevoPrecioPreventa.value,
+      })
+    );
     this.nuevaFechaPreventa.reset();
     this.nuevoPrecioPreventa.reset();
     console.log('preventan', this.presales.controls);
     console.log('arrays', this.arrayItems);
   }
 
-  eliminarPreventa(i: number){
+  eliminarPreventa(i: number) {
     this.presales.removeAt(i);
-    this.arrayItems.splice(i,1);
+    this.arrayItems.splice(i, 1);
     this.demoArray.removeAt(this.demoArray.length - 1);
   }
 
   get demoArray() {
     return this.form.get('demoArray') as FormArray;
- }
+  }
 
- addPresale(presalesC){
-  presalesC.forEach(presale => {
-/*     console.log('presaleCurrent', presale);
+  addPresale(presalesC) {
+    presalesC.forEach((presale) => {
+      /*     console.log('presaleCurrent', presale);
     console.log('presaleCurrent', presale.date_end_presale);
     console.log('pricePresale', presale.price_presale); */
-    this.presales.push(this.fb.control({
-      date_end_presale: presale.date_end_presale,
-      price_presale: presale.price_presale
-    }));
-  })
-  
- }
+      this.presales.push(
+        this.fb.control({
+          date_end_presale: presale.date_end_presale,
+          price_presale: presale.price_presale,
+        })
+      );
+    });
+  }
 }
